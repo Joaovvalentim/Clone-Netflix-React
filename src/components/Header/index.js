f899b62fb37cf1ab9e0a73788906856f95ccfb08
joaovvalentim <joao.victor.valentim@hotmail.com>
2022-09-08 19:13:21 -0300

    Adicionado botões nas listas

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './Header.css';
 
+
 export default ({black}) => {
     return (
         <header className={black ? 'black' : ''}>
--- src/components/MovieRow/index.js
+++ src/components/MovieRow/index.js
@@ -1,10 +1,26 @@
 import React from "react";
 import './MovieRow.css'
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 // eslint-disable-next-line
 export default ({ title, items }) => {
+
+const handleLeftArrow = () =>{
+
+}
+const handleRigthArrow = () =>{
+    
+}
     return (
         <div className="movieRow">
             <h2>{title}</h2>
+            <div className="movieRow--left" onClick={handleLeftArrow}>
+                <NavigateBeforeIcon style={{fontSize:50}}/>
+            </div>
+            <div className="movieRow--rigth"onClick={handleRigthArrow}>
+                <NavigateNextIcon style={{fontSize:50}}/>
+            </div>
+
             <div className="movieRow--listarea">
                 <div className="movieRow--list">
                     {items.results.length > 0 && items.results.map((item, key) => (
