6395283888b075fc89a7dee06e71d469e894d465
joaovvalentim <joao.victor.valentim@hotmail.com>
2022-09-08 18:36:43 -0300

    Css e Navbar implementados

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import Header from "./components/Header";
 export default () => {
 
   const [movieList, setMoiveList] = useState([])
-  const [featureData, setFeatureData] = useState(null)
+  const [featureData, setFeatureData] = useState(null)//
+  const [blackHeader, setBlackHeader] = useState(false)//Verifica se o scroll foi usado para ativar a classe css black no header
 
   useEffect(() => {  //quando minha tela for carregada, esta função será executada
     const loadAll = async () => {
@@ -18,22 +19,38 @@ export default () => {
       setMoiveList(list)
 
       //pegando filme em destaque
-       let originals = list.filter(i=>i.slug==='originals')
-       let randomChosen = Math.floor(Math.random()* (originals[0].items.results.length -1))
-       let chosen = originals[0].items.results[randomChosen]
-       let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv') 
-       setFeatureData(chosenInfo)
+      let originals = list.filter(i => i.slug === 'originals')
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+      let chosen = originals[0].items.results[randomChosen]
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
+      setFeatureData(chosenInfo)
     }
     loadAll()
   }, []);
 
+  useEffect(() => { //monitorar o scroll da pagina para ativar ou nao o css da navbar
+    const scrollListner = () => {
+      if (window.scrollY > 10) { //caso o scroll ja tenha sido rolado, seta true e ativa a clase Css
+        setBlackHeader(true)
+      } else {
+        setBlackHeader(false)// Caso ainda nao, mantem a classe Css como False
+      }
+    }
+    window.addEventListener('scroll', scrollListner)
+
+    return () => {
+      window.removeEventListener('scroll', scrollListner)
+    }
+  }, []);
   return (
     <div className="page">
 
-    <Header/>
+      <Header
+        black={blackHeader}
+      />
 
-      {featureData && 
-      <FeaturedMovie item ={featureData} />
+      {featureData &&
+        <FeaturedMovie item={featureData} />
       }
 
       <section className="lists">
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import './Header.css';
 
-export default () => {
+export default ({black}) => {
     return (
-        <header>
-            ...
+        <header className={black ? 'black' : ''}>
+            <div className="header--logo">
+                <a href="/">
+                    <img src="https://logopng.com.br/logos/netflix-94.png" alt="Netflix"></img>
+                </a>
+            </div>
+            <div className=" header--user">
+                <a href="/">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="usuario"></img>
+                </a>
+            </div>
         </header>
     )
 }
