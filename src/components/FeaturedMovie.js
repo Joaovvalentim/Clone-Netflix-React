2202a747cc418c65fed65cd6eb597882d1063072
joaovvalentim <joao.victor.valentim@hotmail.com>
2022-09-08 17:12:54 -0300

    Pegando todas informações do banner

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import App from './FeaturedMovie.css';
+import './FeaturedMovie.css';
 
 export default ({ item }) => {
+    let firstDate = new Date(item.first_air_date)
+    let genres = []
+    for (let i in item.genres) {
+        genres.push(item.genres[i].name)
+    }
     return (
         <section className='featured' style={{
             backgroundSize: 'cover',
             backgroundPosition: 'center',
             backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
         }}>
-            <div>
-                {item.original_name}
-            </div>
 
-        </section>
+            <div className='feature--vertical'>
+                <div className='feature--horizontal'>
+                    <div className='feature--name'> {item.original_name} </div>
+                    <div className='feature--info'>
+                        <div className='feature--points'>{item.vote_average} pontos</div>
+                        <div className='feature--year'>{firstDate.getFullYear()}</div>
+                        <div className='feature--seasons'> {item.number_of_seasons} Temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
+                    </div>
+                    <div className='feature--description'> {item.overview} </div>
+                    <div className='feature--buttons'>
+                        <a href={`/watch/${item.id}`}>▶ Assistir </a>
+                    <a href={`/list/add/${item.id}`}>+ Minha Lista</a>
+                </div>
+                <div className='feature--genres'><strong>Gêneros:</strong> {genres.join(', ')} </div>
+            </div>
+        </div>
+        </section >
     )
 }
